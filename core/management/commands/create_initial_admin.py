2d6fd4e6fd4b8e9fd583bd12376148d9ac84149f
Danny Jenkins <dannyjenkins71@gmail.com>
2025-12-21 11:35:21 -0500

    Add one-time create_initial_admin command

---
+++ core/management/commands/create_initial_admin.py
@@ -0,0 +1,29 @@
+import os
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Command(BaseCommand):
+    help = "Create initial superuser if none exists"
+
+    def handle(self, *args, **options):
+        if User.objects.filter(is_superuser=True).exists():
+            self.stdout.write("Superuser already exists.")
+            return
+
+        username = os.getenv("DJANGO_ADMIN_USERNAME")
+        email = os.getenv("DJANGO_ADMIN_EMAIL")
+        password = os.getenv("DJANGO_ADMIN_PASSWORD")
+
+        if not all([username, email, password]):
+            self.stdout.write("Admin env vars not set. Skipping.")
+            return
+
+        User.objects.create_superuser(
+            username=username,
+            email=email,
+            password=password
+        )
+
+        self.stdout.write("Initial superuser created.")
