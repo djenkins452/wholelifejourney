df2c1a31363a9d84858c3ca5350465f30d57c734
Danny Jenkins <dannyjenkins71@gmail.com>
2025-12-21 11:54:32 -0500

    Harden admin URL

--- wlj/urls.py
+++ wlj/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 from .views import home
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("wlj-super-duper/", admin.site.urls),
     path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("", home, name="home"),
